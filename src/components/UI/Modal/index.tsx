2c48b938b891993ba6f1bc05993fab3aafb57ab6
vladji <vladjislav@gmail.com>
2024-02-28 19:18:29 +0600

    feat: modal window

---
+++ src/components/UI/Modal/index.tsx
@@ -0,0 +1,47 @@
+import { FC, ReactNode, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import styles from './styles.module.scss';
+import { CardBlank } from '../CardBlank';
+import { CrossIcon } from '../../../icons/CrossIcon';
+
+interface Props {
+  visible: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const MODAL_ID = 'modal-wrapper';
+
+export const Modal: FC<Props> = ({ visible, onClose, children }) => {
+  useEffect(() => {
+    if (visible) {
+      document.body.style.overflow = 'hidden';
+    }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [visible]);
+
+  const handleClose = (e: any) => {
+    const elementId = e.target.id;
+    if (elementId === MODAL_ID) {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      {visible && createPortal(
+        <div id={MODAL_ID} onClick={handleClose} className={styles.modalWrapper}>
+          <CardBlank className={styles.card}>
+            <button className={styles.closeButton} onClick={onClose}>
+              <CrossIcon />
+            </button>
+            {children}
+          </CardBlank>
+        </div>,
+        document.body
+      )}
+    </>
+  );
+};
