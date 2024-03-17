30b2290a03b6c2b9bec7e4b2bf06cc63a0c779b4
vladji <vladjislav@gmail.com>
2024-03-17 20:20:48 +0500

    feat: redirect notify modal

--- src/components/MakePayment/index.tsx
+++ src/components/MakePayment/index.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { CardBlank } from '../UI/CardBlank';
 import { MainButton } from '../UI/MainButton';
+import { Modal } from '../UI/Modal';
+import styles from './styles.module.scss';
 
 export const MakePayment = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   const makePayment = async () => {
-    document.location.href = 'https://customer.freedompay.money/links?slug=H4hK2'
+    document.location.href = 'https://customer.freedompay.money/links?slug=H4hK2';
   };
 
   return (
-    <CardBlank>
-      <MainButton text="Внести оплату" handler={makePayment} />
-    </CardBlank>
+    <>
+      <CardBlank>
+        <MainButton text="Внести оплату" handler={() => setShowModal(true)} />
+      </CardBlank>
+      <Modal visible={showModal} onClose={() => setShowModal(false)}>
+        <div className={styles.modalWrapper}>
+          <p>Вы будете перенаправлены на страницу оплаты.<br /> В поле <b>"Номер заказа"</b> введите <b>свой ИИН</b></p>
+          <MainButton text="Оплатить" handler={makePayment} />
+        </div>
+      </Modal>
+    </>
   );
 };
